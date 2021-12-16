a3ec5cea882f76f896cdd3bd9e2b3bfa5f6ffa1b
Jon Erik <jon.erik.ullvang@gmail.com>
2021-12-16 23:41:06 +0100

    starter

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+    <>
+      10,000+ of our users love our products. We only provide great products
+      combined with excellent customer service. See what our satisfied customers
+      are saying about our services. Rated 5 Stars in Reviews Rated 5 Stars in
+      Report Guru Rated 5 Stars in BestTech Colton Smith Verified Buyer "We
+      needed the same printed design as the one we had ordered a week prior. Not
+      only did they find the original order, but we also received it in time.
+      Excellent!" Irene Roberts Verified Buyer "Customer service is always
+      excellent and very quick turn around. Completely delighted with the
+      simplicity of the purchase and the speed of delivery." Anne Wallace
+      Verified Buyer "Put an order with this company and can only praise them
+      for the very high standard. Will definitely use them again and recommend
+      them to everyone!"
+      <div className="attribution">
+        Challenge by
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="https://www.frontendmentor.io?ref=challenge"
           target="_blank"
-          rel="noopener noreferrer"
+          rel="noreferrer"
         >
-          Learn React
+          Frontend Mentor
         </a>
-      </header>
-    </div>
+        . Coded by <a href="https://github.com/Ullvang">Jon Erik Ullvang</a>.
+      </div>
+    </>
   );
 }
 
